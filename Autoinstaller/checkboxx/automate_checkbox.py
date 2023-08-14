087c15c1eb6ebc9f585ec473866e8d58fb78d4e0
karthik <130954788+karthikvvk@users.noreply.github.com>
2023-08-14 13:16:24 +0530

    Update automate_checkbox.py

--- Autoinstaller/checkboxx/automate_checkbox.py
+++ Autoinstaller/checkboxx/automate_checkbox.py
@@ -1,13 +1,9 @@
 """
-Creates checkboxes using the given sequence and return the checkbox objects in 'vari'
-Also can get the checked items using fetch_cked_val(); return as list
-That is 'print(cr_checkbox(tk_window_object, sequence)[1])' will print all the checkbox's objects.
-And
-
-for i in cr_checkbox(tk_window_object, sequence)[1]:
-        print(i.get())
-
-the above will print the value(1 or 0), that is the status of checked(1) or unchecked(0).
+cr_checkbox(): takes the tkinter window object and the items to be listed as arguments and return checkbox objects as list.
+fetch_cked_val(): takes the list returned by cr_checkbox() as argument and return a list of checked/selected items.
+**NOTE: ONLY WORK FOR TKINTER.
+    >Default  layout managers is .grid()
+    >Please change according  to your prefered layout managers(.place(), .pack(), etc..).
 """
 from tkinter import *
 
